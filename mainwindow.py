64cbee7b508520ba78dc07770ffda8e6e96ad293
BENIS Jean <j6benis@laurier2.enib.fr>
2021-05-18 10:22:15 +0200

    Create text dialog and import changes from jupyter

--- mainwindow.py
+++ mainwindow.py
@@ -6,7 +6,8 @@ from PyQt5.QtCore import Qt, QSize, QFile, QIODevice
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, \
 QToolBar,QAction,QStatusBar,QGraphicsView, \
-QLabel,QVBoxLayout,QDialog, QDialogButtonBox, QColorDialog, QMenu, QMessageBox, QFileDialog
+QLabel,QVBoxLayout,QDialog, QDialogButtonBox, \
+QColorDialog, QMenu,QMessageBox, QFileDialog
 
 from scene import Scene
 
@@ -18,6 +19,7 @@ class MainWindow(QMainWindow):
         self.create_actions()
         self.create_toolbars()
         self.create_menus()
+        self.resize(1080,800)
     def create_scene(self) :
         view=QGraphicsView()
         self.scene=Scene(self)
@@ -52,32 +54,85 @@ class MainWindow(QMainWindow):
         self.action_style_brush_color=QAction(QIcon('Icons/monkey_on_16x16.png'), name, self)
         self.action_style_brush_color.setStatusTip("Select Brush color")
         self.action_style_brush_color.triggered.connect(lambda status,selection=name : self.on_triggered_action(status,selection))
+        name="Rectangle"
+        self.action_rectangle=QAction(QIcon('Icons/tool_rectangle.png'), name, self)
+        self.action_rectangle.setStatusTip("Create a rectangle")
+        self.action_rectangle.setCheckable(True)
+        self.action_rectangle.triggered.connect(lambda status,selection=name :
+                                                self.on_triggered_action(status,selection))
+        name="Ellipse"
+        self.action_ellipse=QAction(QIcon('Icons/tool_ellipse.png'), name, self)
+        self.action_ellipse.setStatusTip("Create an ellipse")
+        self.action_ellipse.setCheckable(True)
+        self.action_ellipse.triggered.connect(lambda status,selection=name :
+                                                self.on_triggered_action(status,selection))
+        name="Line"
+        self.action_line=QAction(QIcon('Icons/tool_line.png'), name, self)
+        self.action_line.setStatusTip("Create a line")
+        self.action_line.setCheckable(True)
+        self.action_line.triggered.connect(lambda status,selection=name :
+                                                self.on_triggered_action(status,selection))
+        name="Polygon"
+        self.action_polygon=QAction(QIcon('Icons/tool_polygon.png'), name, self)
+        self.action_polygon.setStatusTip("Create a line")
+        self.action_polygon.setCheckable(True)
+        self.action_polygon.triggered.connect(lambda status,selection=name :
+                                                self.on_triggered_action(status,selection))
+        name="Text"
+        self.action_text=QAction(QIcon('Icons/tool_text.png'), name, self)
+        self.action_text.setStatusTip("Create a line")
+        self.action_text.setCheckable(True)
+        self.action_text.triggered.connect(lambda status,selection=name :
+                                                self.on_triggered_action(status,selection))
     def create_toolbars(self) :
         self.toolbar=QToolBar("Main Toolbar")
         self.addToolBar(self.toolbar)
         self.toolbar.setIconSize(QSize(16,16))
+        self.setStatusBar(QStatusBar(self))
+
         self.toolbar.addAction(self.action_file_new)
         self.toolbar.addAction(self.action_file_open)
         self.toolbar.addAction(self.action_file_exit)
+
+        self.toolbar.addAction(self.action_line)
+        self.toolbar.addAction(self.action_rectangle)
+        self.toolbar.addAction(self.action_ellipse)
+        self.toolbar.addAction(self.action_polygon)
+        self.toolbar.addAction(self.action_text)
     def create_menus(self) :
         menubar=self.menuBar()
         self.menu_file = menubar.addMenu("&File")
         self.menu_style = menubar.addMenu("&Style")
+        self.menu_tools = menubar.addMenu("&Tools")
+
         self.pen_style = QMenu('Pen',self)
         self.menu_style.addMenu(self.pen_style)
         self.brush_style = QMenu('Brush',self)
         self.menu_style.addMenu(self.brush_style)
+
+
         self.menu_file.addAction(self.action_file_new)
         self.menu_file.addAction(self.action_file_open)
         self.menu_file.addAction(self.action_file_save)
         self.menu_file.addAction(self.action_file_exit)
+
         self.pen_style.addAction(self.action_style_pen_color)
         self.brush_style.addAction(self.action_style_brush_color)
+
+        self.menu_tools.addAction(self.action_line)
+        self.menu_tools.addAction(self.action_rectangle)
+        self.menu_tools.addAction(self.action_ellipse)
+        self.menu_tools.addAction(self.action_polygon)
+        self.menu_tools.addAction(self.action_text)
      
     def on_triggered_action(self,status,selection):
         print("status:",status,", selection:",selection)
         if selection=="Exit" :
             self.exit()
+        elif selection=="New" :
+            self.new()
+        elif selection=="Save As" :
+            self.save()
         elif selection=="Pen Color" :
             color=self.style_color()
             if color :
@@ -86,10 +141,38 @@ class MainWindow(QMainWindow):
             color=self.style_color()
             if color :
                 self.scene.set_brush_color(color)
-        elif selection=="New" :
-            self.new();
-        elif selection=="Save As" :
-            self.save();
+        elif selection=="Line" : 
+            self.scene.set_tool("line")
+            self.action_rectangle.setChecked(False)
+            self.action_ellipse.setChecked(False)
+            self.action_polygon.setChecked(False)
+            self.action_text.setChecked(False)
+        elif selection=="Rectangle" : 
+            self.scene.set_tool("rect")
+            self.action_line.setChecked(False)
+            self.action_ellipse.setChecked(False)
+            self.action_polygon.setChecked(False)
+            self.action_text.setChecked(False)
+        elif selection=="Ellipse" : 
+            self.scene.set_tool("ellipse")
+            self.action_rectangle.setChecked(False)
+            self.action_line.setChecked(False)
+            self.action_polygon.setChecked(False)
+            self.action_text.setChecked(False)
+        elif selection=="Polygon" : 
+            self.scene.set_tool("polygon")
+            self.action_rectangle.setChecked(False)
+            self.action_ellipse.setChecked(False)
+            self.action_line.setChecked(False)
+            self.action_text.setChecked(False)
+        elif selection=="Text" : 
+            self.scene.set_tool("text")
+            self.action_rectangle.setChecked(False)
+            self.action_ellipse.setChecked(False)
+            self.action_polygon.setChecked(False)
+            self.action_line.setChecked(False)
+
+
 
     def new(self):
         popup = QMessageBox.warning(self,"New","Are you sure you want to open a new file? \nUnsaved changes will be ignored.", QMessageBox.Ok,QMessageBox.Cancel)
